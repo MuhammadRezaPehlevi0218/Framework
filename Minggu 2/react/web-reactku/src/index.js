acaffd9ee89ecf7482735354fcd2894a27e6a357
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2022-03-02 03:23:01 +0700

    prak 1

--- Minggu 2/react/web-reactku/src/index.js
+++ Minggu 2/react/web-reactku/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const Hello=()=>{
-  return <p>Hello world</p>
-}
+function HelloComponent(){
+  return HelloComponent
+};
 
-ReactDOM.render(<Hello/>,document.getElementById('root'));
+ReactDOM.render(<HelloComponent/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
