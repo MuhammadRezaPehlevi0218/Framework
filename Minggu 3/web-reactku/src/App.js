0a63fee3199a56860723e0d8b822c09ad7197e1e
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2022-03-02 09:49:25 +0700

    list

--- Minggu 3/web-reactku/src/App.js
+++ Minggu 3/web-reactku/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';// Component menggunakan Functionconst Footer = 
-(props) => {
-return (
- <div>
- <h3>Halaman Footer</h3>
- <h3>Component ini dibuat menggunakan Function bukan Class</h3>
- <p>Nilai ini ditampilkan dari props: { props.judul } </p>
- <p>Nama Saya: { props.nama } </p>
- </div>
-);
-}
-export default Footer;
+import React, { Component } from 'react';
+import Header from './component/footer';
+import Footer from './component/footer';
+import List from './component/list'; 
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <h1>Component dari Class App</h1>
+                <List />
+                <Footer judul='Halaman Footer' nama='Reza' />
+            </div>
+        );
+    }
+} export default App;
+
--- Minggu 3/web-reactku/src/index.js
+++ Minggu 3/web-reactku/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 function HelloComponent(){
@@ -14,7 +14,7 @@ class StateFullCOmponent extends React.Component{
   }
 }
 
-ReactDOM.render(<HelloComponent/>,document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
