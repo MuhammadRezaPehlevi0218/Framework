2ef047d849d919e030ca44e60f349e5b52c1e5f2
MuhammadRezaPehlevi0218 <reyza99@gmail.com>
2022-06-03 00:36:20 +0700

    js 11-12

--- UAS_PBF/big-project/src/firebase/firebase.js
+++ UAS_PBF/big-project/src/firebase/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-z59wAdlVZd8salCGqxbo2-WxwwSHous",
-  authDomain: "minggu11-c3844.firebaseapp.com",
-  projectId: "minggu11-c3844",
-  storageBucket: "minggu11-c3844.appspot.com",
-  messagingSenderId: "68827573142",
-  appId: "1:68827573142:web:a46799612ccab680b08bb7",
-  measurementId: "G-CYMB4HGYX6"
+  apiKey: "AIzaSyANUF4_iLEVMOrHEALFZrlSpkAxPchpBvs",
+  authDomain: "big-project-pbf.firebaseapp.com",
+  projectId: "big-project-pbf",
+  storageBucket: "big-project-pbf.appspot.com",
+  messagingSenderId: "885364667419",
+  appId: "1:885364667419:web:24917de4c30948b92ff4be",
+  measurementId: "G-NGH4P1ERH7"
   };
 export const myFirebase = firebase.initializeApp(firebaseConfig);
 const baseDb = myFirebase.firestore();
